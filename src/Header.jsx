74f88ac5603ba79741c699e41f92696f3700c701
BuwieSit <santosbuhawie28@gmail.com>
2025-02-24 06:52:09 +0800

    first

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+
+        <header class="w-screen h-30 bg-red-500">
+            <h1 class="fill-white font-bold text-2xl" >My websitenings</h1>
+            <nav>
+                <ul>
+                    <li>Test</li>
+                </ul>
+            </nav>
+            
+        </header>
+    );
+}
+
+export default Header
