34e4fc7efbf4dd2614502eafdcd19fb14dee0149
BuwieSit <santosbuhawie28@gmail.com>
2025-02-25 20:57:17 +0800

    footer

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer() {
+
+    return (
+        <footer class="flex-col items-center justify-center">
+            <div class=" w-screen h-30 bg-red-50 bottom-full flex-col items-center justify-center text-center">
+                <p>All rights reserved</p>
+                <p>Buhawiesit Works</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,18 +2,8 @@
 function Header() {
     return (
 
-        <header class="w-screen h-30 bg-red-500">
-            <h1 class="fill-white font-bold text-2xl" >My websitenings</h1>
-            <nav>
-                <ul>
-                    <li>Test</li>
-                    <li>Test 2</li>
-                    <li>Test 3</li>
-                    <li>Test 4</li>
-                    <li>Test 5</li>
-                    <li>Test 6</li>
-                </ul>
-            </nav>
+        <header class="w-screen h-30 bg-red-500 text-center">
+            <h1 class="fill-white font-bold text-5xl" >My websitenings</h1>
             
         </header>
     );
